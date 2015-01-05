8dd92ecea0c1cde086dd328cb135dcb6a1adbc61
Sebastian Esp <private@sebastianesp.dk>
2015-01-05 12:42:07 +0100

    Euler Project - Challenge #7

---
+++ Prime Calculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("which prime number do you want to find?");
+            string num = Console.ReadLine();
+            int numInt;
+            Int32.TryParse(num, out numInt);
+            numInt -= 1;
+
+            List<int> primeNumbers = new List<int>();
+            bool isPrime = true;
+
+            for (int i = 2; i <= 200000; i++)
+            {
+                for (int j = 2; j <= 200000; j++)
+                {
+
+                    if (i != j && i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+
+                }
+                if (isPrime)
+                {
+                    primeNumbers.Add(i);
+                }
+                isPrime = true;
+            }     
+
+            primeNumbers.ForEach(y => Console.WriteLine("{0}", y));
+            Console.WriteLine("\nThe 10001st prime number is: {0}",primeNumbers[numInt].ToString());
+            Console.Read();
+        }
+    }
+}
